bde99218d31046160243144c6e66c37a9f9ddfab
r4z3c <cezar.almeidajr@gmail.com>
2017-02-21 19:28:38 -0300

    - improving readability - improving tests - fixing the loss of method access restriction after override

--- lib/method_decorator/decoration.rb
+++ lib/method_decorator/decoration.rb
@@ -1,11 +1,11 @@
 module MethodDecorator
   class Decoration
 
-    attr_accessor :target_class, :target_name, :target_method, :decoration
+    attr_accessor :target_class, :target_method_name, :target_method, :decoration
 
-    def initialize(target_class, target_method_name, target_method)
+    def initialize(target_class, target_method_name, target_method, &decoration)
       self.target_class = target_class
-      self.target_name = target_method_name
+      self.target_method_name = target_method_name
       self.target_method = target_method
       self.decoration = block_given? ? Proc.new : nil
     end
--- spec/lib/method_decorator_spec.rb
+++ spec/lib/method_decorator_spec.rb
@@ -1,26 +1,116 @@
 require 'spec_helper'
-require 'support/some_class'
+require 'support/dummy_class'
 
 describe MethodDecorator do
 
-  let(:some_arg) { :some_arg }
+  let(:dummy_arg) { :dummy_arg }
 
-  subject { target.some_method some_arg }
+  context 'when common class as target' do
 
-  before do
-    expect(target).to receive(:puts).with(:decorated_some_method).ordered
-    expect(target).to receive(:puts).with(some_arg).ordered
+    let(:target) { DummyClass }
+    let(:target_instance) { target.new }
+
+    context 'when public method' do
+
+      before { expect(target_instance).to receive(:puts).with(:a_decorated_public_instance_method).ordered }
+      before { expect(target_instance).to receive(:puts).with("a_public_instance_method_arg: #{dummy_arg}").ordered }
+
+      it { target_instance.a_public_instance_method dummy_arg }
+
+    end
+
+    context 'when protected method' do
+
+      context 'when calling protected method deliberately' do
+
+        it { expect{target_instance.a_protected_instance_method dummy_arg}.to raise_error NoMethodError }
+
+      end
+
+      context 'when calling protected method through `send`' do
+
+        before { expect(target_instance).to receive(:puts).with(:a_decorated_protected_instance_method).ordered }
+        before { expect(target_instance).to receive(:puts).with("a_protected_instance_method_arg: #{dummy_arg}").ordered }
+
+        it { target_instance.send :a_protected_instance_method, dummy_arg }
+
+      end
+
+    end
+
+    context 'when private method' do
+
+      context 'when calling private method deliberately' do
+
+        it { expect{target_instance.a_private_instance_method dummy_arg}.to raise_error NoMethodError }
+
+      end
+
+      context 'when calling private method through `send`' do
+
+        before { expect(target_instance).to receive(:puts).with(:a_decorated_private_instance_method).ordered }
+        before { expect(target_instance).to receive(:puts).with("a_private_instance_method_arg: #{dummy_arg}").ordered }
+
+        it { target_instance.send :a_private_instance_method, dummy_arg }
+
+      end
+
+    end
+
   end
 
-  context 'when singleton method context' do
-    let(:target) { SomeClass }
-    it { subject }
-  end
+  context 'when singleton class as target' do
 
-  context 'when instance method context' do
-    let(:target) { SomeClass.new }
-    let(:methods) { target.class.send :generated_methods }
-    it { subject }
+    let(:target) { DummyClass.singleton_class }
+    let(:target_instance) { DummyClass }
+
+    context 'when public method' do
+
+      before { expect(target_instance).to receive(:puts).with(:a_decorated_public_singleton_method).ordered }
+      before { expect(target_instance).to receive(:puts).with("a_public_singleton_method_arg: #{dummy_arg}").ordered }
+
+      it { target_instance.a_public_singleton_method dummy_arg }
+
+    end
+
+    context 'when protected method' do
+
+      context 'when calling protected method deliberately' do
+
+        it { expect{target_instance.a_protected_singleton_method dummy_arg}.to raise_error NoMethodError }
+
+      end
+
+      context 'when calling protected method through `send`' do
+
+        before { expect(target_instance).to receive(:puts).with(:a_decorated_protected_singleton_method).ordered }
+        before { expect(target_instance).to receive(:puts).with("a_protected_singleton_method_arg: #{dummy_arg}").ordered }
+
+        it { target_instance.send :a_protected_singleton_method, dummy_arg }
+
+      end
+
+    end
+
+    context 'when private method' do
+
+      context 'when calling private method deliberately' do
+
+        it { expect{target_instance.a_private_singleton_method dummy_arg}.to raise_error NoMethodError }
+
+      end
+
+      context 'when calling private method through `send`' do
+
+        before { expect(target_instance).to receive(:puts).with(:a_decorated_private_singleton_method).ordered }
+        before { expect(target_instance).to receive(:puts).with("a_private_singleton_method_arg: #{dummy_arg}").ordered }
+
+        it { target_instance.send :a_private_singleton_method, dummy_arg }
+
+      end
+
+    end
+
   end
 
 end
